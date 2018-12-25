41a850b670dbf118ae7b894e567b301951567a9f
ahmed <ahmedmostafa05031991@gmail.com>
2018-12-25 08:23:07 +0200

    modified assignment page two

--- js/main.js
+++ js/main.js
@@ -43,3 +43,92 @@ function autoHeightAnimate(element, time){
     	element.height(curHeight); // Reset to Default Height
     	element.stop().animate({ height: autoHeight }, time); // Animate to Auto Height
 }
+
+// box 1
+
+new Chart(document.getElementById("line-chart1"), {
+    type: 'line',
+    data: {
+      labels: ["","","","","","","","","",""],
+      datasets: [{ 
+          data: [45,53,46,55,58,52,80,49,54,51],
+          label: "Posts",
+          borderColor: "#062886",
+          fill: false,
+          pointBorderWidth: 3,
+          pointHoverRadius: 7,
+          pointHoverBorderWidth: 1,
+          pointRadius: 3,
+        }, { 
+          data: [9,79,54,50,48,9,48,25,47,70],
+          label: "Comments",
+          borderColor: "#4C78F2",
+          fill: false,
+          pointBorderWidth: 3,
+          pointHoverRadius: 7,
+          pointHoverBorderWidth: 1,
+          pointRadius: 3,
+        },
+      ]
+    },
+    options: {
+      bezierCurve: false,
+      title: {
+        display: true,
+      },
+      elements: {
+          line: {
+              tension: 0
+          }
+      }
+    }
+  });
+
+// box 2
+
+new Chart(document.getElementById("line-chart2"), {
+    type: 'line',
+    data: {
+      labels: ["","","","","","","","","",""],
+      datasets: [{ 
+          data: [25,94,86,80,62,23,70],
+          label: "Natural",
+          borderColor: "#F4F071",
+          fill: false,
+          pointBorderWidth: 3,
+          pointHoverRadius: 7,
+          pointHoverBorderWidth: 1,
+          pointRadius: 3,
+        }, { 
+          data: [32, 52,28,60,70,53,75,48,57],
+          label: "Positive",
+          borderColor: "#51FE7F",
+          fill: false,
+          pointBorderWidth: 3,
+          pointHoverRadius: 7,
+          pointHoverBorderWidth: 1,
+          pointRadius: 3,
+        }, { 
+          data: [9,71,54,50,40,8,48,15,47,80],
+          label: "Negative",
+          borderColor: "#B90B0B",
+          fill: false,
+          pointBorderWidth: 3,
+          pointHoverRadius: 7,
+          pointHoverBorderWidth: 1,
+          pointRadius: 3,
+        } 
+      ]
+    },
+    options: {
+      bezierCurve: false,
+      title: {
+        display: true,
+      },
+      elements: {
+          line: {
+              tension: 0
+          }
+      }
+    }
+  });;
